6f95eb991aa5be8ea974afc5ebae65df5c5d259e
phamduchongan93 <vincentpham1993@gmail.com>
2023-01-25 06:28:52 -0800

    update bluetooth string parser

--- bluetooth_audio_patch.sh
+++ bluetooth_audio_patch.sh
@@ -3,29 +3,67 @@
 # Author: An Pham
 # Date: Sun 19 Jun 2022 10:15:08 PM PDT
 
-blue_headset='E8_EE_CC_23_BD_5A'
+# Add or change bluetooth device name here
+blue_headset='3B:64:D2:73:B4:C3'
+
+sudo_checker () {
+  pass
+}
+
+string_convert () {
+  local input_string="$1"
+  # convert the '3B:64:D2:73:B4:C3' string to 3B_64_D2_73_B4_C3
+  echo "$input_string" | sed -e 's/:/_/g'
+}
 
 connect_bluetooth () {
   local blue_device="$(echo "$blue_headset" | sed "s/_/:/g") "
-  bluetoothctl -- connect  $blue_device; pactl set-sink-volume 0 20%
+  bluetoothctl -- connect  $blue_headset; pactl set-sink-volume 0 20% 
+}
+
+setting_audio_profile () {
+  pactl set-sink-volume 0 20% 
 }
 
 disconnect_bluetooth () {
   local blue_device="$(echo "$blue_headset" | sed "s/_/:/g") "
-  bluetoothctl -- disconnect $blue_device; pactl set-sink-volume 0 0%
+  bluetoothctl -- disconnect $blue_headset; pactl set-sink-volume 0 0%
 }
 
 restart_bluetooth () {
   disconnect_bluetooth 
-  systemctl restart bluetooth
+  systemctl --user daemon-reload
   connect_bluetooth
+}
+
+install_dependency () {
+  add-apt-repository ppa:pipewire-debian/pipewire-upstream
+  apt-get update
+  apt install pipewire pipewire-audio-client-libraries
+  apt install gstreamer1.0-pipewire libpipewire-0.3-{0,dev,modules} libspa-0.2-{bluetooth,dev,jack,modules} pipewire{,-{audio-client-libraries,pulse,media-session,bin,locales,tests}}
+}
+
+install_notifier () {
+  apt install dunst
+}
+
+repatch_bluetooth () {
+  apt install libspa-0.2-bluetooth 
+  systemctl --user --now disable pulseaudio.service pulseaudio.socket
+  systemctl --user --now disable pulseaudio.service 
+  systemctl --user --now disable pulseaudio.socket 
+  systemctl --user mask pulseaudio
+  systemctl --user --now enable pipewire-media-session.service
+  systemctl --user restart pipewire
 }
 
 set_audio_only () {
   # add profile to the card 
-  pactl set-card-profile "bluez_card.$blue_headset" a2dp-sink
+  local bluetooth_dev="$(string_convert $blue_headset)"
+  
+  pactl set-card-profile "bluez_card.$bluetooth_dev" a2dp-sink && notify-send "Bluetooth headset has been switched to audio mode" || notify-send "Volume has been switched to audio mode only"
   pactl set-sink-volume 0 70%
-  echo "Volme has been switched to audio mode"
+  notify-send "Volume has been switched to 70 percent"
 }
 
 check_if_device_available () {
@@ -40,9 +78,12 @@ check_if_profile_available () {
 
 set_mic_only () {
   # add mic and audion 
+  bluetooth_dev="$(string_convert $blue_headset)"
+
   pactl set-card-profile "bluez_card.$blue_headset" headset-head-unit
   pactl set-sink-volume 0 40%
   echo "Volume has been switched to audio and mic mode"
+  notify-send "Volume has been switched to audio and mic mode"
 }
 
 main () {
@@ -51,6 +92,10 @@ main () {
      case "$1" in
        --reset | -r)
 	 restart_bluetooth
+	 shift
+	 ;;
+       --install | -i)
+	 install_dependency
 	 shift
 	 ;;
        --version | -v)
@@ -65,11 +110,11 @@ main () {
 	 shift
 	 ;;
        --audio | -a)
-	 set_audio_only 
+	 connect_bluetooth && set_audio_only || echo "Fail: Unable to connect the bluetooth device"
 	 shift
 	 ;;
        --mic | -ac)
-	 set_mic_only
+	 connect_bluetooth && set_mic_only || echo "Fail: Unable to connect the bluetooth device"
 	 shift
 	 ;;
        * | -*)
@@ -83,7 +128,7 @@ main () {
 }
 
 help () {
-  echo "Usuage: $(basename $0) <-c|-r|-l> "
+  echo "Usuage: $(basename $0) <-c|-d|-a|-ac>. First, edit the variable to match your bluetooth interface MAC  address. "
   echo ''
   echo 'Where:'
   echo '  -c,--check       check if the bluetooth headset is connected'
@@ -92,6 +137,7 @@ help () {
   echo '  -d,--disconnect  disconnect bluetooth headset'
   echo '  -a,--audio       connect with audio only'
   echo '  -ac,--audio-mic  connect with both audio and mic'
+  echo '  -i,--install	   install dependency'
   echo '' 
   echo 'Example:'
   echo "  $(basename $0) -c | --check   Check if the device is connected"
